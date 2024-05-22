4020cc73a3070b58ac5a2df25968837f99548c6b
Vicente <vincentius.ptj@gmail.com>
2024-05-22 18:31:02 -0300

    Template + 2 PowerUps criados

--- Jogador/jogador.gd
+++ Jogador/jogador.gd
@@ -8,51 +8,59 @@ class_name Jogador
 @onready var Animações := $Animação
 
 enum Estado{parado,	morto, movendo}
-var EstadoAtual := Estado.parado
-var EstaVivo := true
-
-var podeAtirar := true
+var estadoAtual : Estado= Estado.parado
+var estaVivo : bool= true
+var tiroTriplo : bool = false
+var podeAtirar : bool = true
+var misselExplosivo: bool = false
+var componenteAtaque: Node
 
 func _ready():
-	$VidaComponente.connect("Morto", self.Is_vivo)
+	$VidaComponente.connect("Morto", self.is_vivo)
 	pass
 
 func _physics_process(_delta):
 	var directionX = Input.get_axis("esq", "dir")	
 	var directionY = Input.get_axis("cima", "baixo")
-
-	# Very ugly State Machine
-	if !EstaVivo:
-		EstadoAtual = Estado.morto
-	elif directionX != 0 or directionY != 0:
-		EstadoAtual = Estado.movendo
-	elif directionX == 0 and directionY == 0:
-		EstadoAtual = Estado.parado
 	
-	if EstadoAtual == Estado.movendo:
-		if directionY:
-			velocity.y = directionY * Speed
-
+	# Very ugly State Machine
+	if !estaVivo:
+		estadoAtual = Estado.morto
+	elif directionX != 0 or directionY != 0:
+		estadoAtual = Estado.movendo
+	elif directionX == 0 and directionY == 0:
+		estadoAtual = Estado.parado
+	
+	if estadoAtual == Estado.movendo:
+		
+		velocity.y = directionY * Speed
+		velocity.x = directionX * Speed
 		if directionX > 0:
-			Animações.play("direita")
-			velocity.x = directionX * Speed
+			Animações.play("direita")		
 		elif directionX < 0:
-			Animações.play("esquerda")
-			velocity.x = directionX * Speed			
+			Animações.play("esquerda")		
 		move_and_slide()
-	elif EstadoAtual == Estado.parado:
+	elif estadoAtual == Estado.parado:
 		Animações.play("idle")
 		velocity = Vector2(0,0)	
-	elif EstadoAtual == Estado.morto:
+	elif estadoAtual == Estado.morto:
 		Animações.play("morte")
 		await get_tree().create_timer(1).timeout
 		get_tree().change_scene_to_file("res://UI/DeathMenu.tscn")
 	
 	if Input.is_action_pressed("tiro") and podeAtirar:
-		get_node("AtaqueComponente").Ataque(position.x-8,position.y)
-		get_node("AtaqueComponente").Ataque(position.x+8,position.y)
+		if misselExplosivo:
+			componenteAtaque = get_node("MisselComponente")
+		else:	
+			componenteAtaque = get_node("AtaqueComponente")
+		componenteAtaque.Ataque(position.x,position.y)
+		if tiroTriplo:
+			componenteAtaque.Ataque(position.x+10,position.y)
+			componenteAtaque.Ataque(position.x-10,position.y)
+			
 		podeAtirar = false
 		$AtaqueDelay.start()
+	
 	
 	position = position.clamp(Vector2(8, 8), screensize-Vector2(8, 8))
 	pass
@@ -60,5 +68,22 @@ func _physics_process(_delta):
 func _on_ataque_delay_timeout():
 	podeAtirar = true
 
-func Is_vivo():
-	EstaVivo = false
+func _power_up(id: int) -> void:
+	if id == 0:
+		tiroTriplo = true
+		$TiroTriploDelay.start()
+	elif id == 2:
+		misselExplosivo = true
+		$MisselExplosivoDelay.start()
+	pass
+
+func is_vivo():
+	estaVivo = false
+
+
+func _on_tiro_triplo_delay_timeout():
+	tiroTriplo = false
+
+
+func _on_missel_explosivo_delay_timeout():
+	misselExplosivo = false
---
+++ Projeteis/Jogador/Missel.gd
@@ -0,0 +1,27 @@
+extends ProjetilComp
+@onready var Animacao = $AnimationPlayer
+
+func _ready():
+	Animacao.play("Missel")
+	$ExplosaoTimer.start()
+	$Desaparecer.start()
+	pass # Replace with function body.
+
+func _process(delta):
+	position -= Vector2(0, 1).rotated(0) * delta * Velocidade
+	pass
+
+func _on_area_entered(area:Area2D):
+	if area is HitboxComp:
+		area.Hit(Dano)
+	pass
+
+
+func _on_desaparecer_timeout():
+	queue_free()
+	pass # Replace with function body.
+
+
+func _on_explosao_timer_timeout():
+	Velocidade = 70
+	pass # Replace with function body.
