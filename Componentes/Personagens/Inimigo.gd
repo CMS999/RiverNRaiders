046259319aa4d475ab509c21f315acb63686a5f7
JeanLucasR <jean@estudante.ufscar.br>
2024-06-10 15:13:05 -0300

    Missil, classe inimigo e aviao

--- Componentes/Personagens/Inimigo.gd
+++ Componentes/Personagens/Inimigo.gd
@@ -1,12 +1,20 @@
 extends Node2D
 class_name Inimigo
 
-@export var vida: int = 100
-@export var velocidade: float = 100.0
-@export var fire_rate: float = 1.0
+@export var vida: int = 1
+@export var velocidade: float = 0.15
 @export var dano_colisao: int = 10
+@export var fire_rate: float = 1.0
+
+
+
+@onready var GlobalReference = get_node("/root/GlobalValues")
+var Alvo 
+var Direcao :Vector2 = Vector2(1,0)
+var flag :bool= true
 
 # Referências aos nós filhos
+@onready var ataque_inimigo = $AtaqueInimigo
 @onready var fire_timer = $FireTimer
 @onready var ataque_comp = $AtaqueComp
 @onready var vida_comp = $VidaComp
@@ -17,36 +25,42 @@ class_name Inimigo
 signal enemy_destroyed
 
 var pode_atirar: bool = true
-var alvo : Node
 
 func _ready():
+	$Animações.flip_h = true
+	Alvo = GlobalReference.JogadorRef
 	$VidaComp.connect("Morto", self.Morrer)
 	$Timer.connect("timeout", self._on_timer_timeout)
 	$Hurtbox.connect("area_entered", self._on_area_entered)
+	$fire_timer.connect("timeout", self._on_timer_timeout)
 	fire_timer.wait_time = fire_rate  # Configura o tempo de espera do Timer com o fire_rate
 	fire_timer.start()  # Inicia o Timer
-	$fire_timer.connect("timeout", self._on_timer_timeout)
+	
+	
 
 func _process(delta):
-	position.y += velocidade * delta
-	if position.y > get_viewport_rect().size.y:
-		queue_free()
-
-	if pode_atirar and ataque_comp.projetil:
-		ataque_comp.Ataque(global_position.x, global_position.y)
-		pode_atirar = false
-
-func _on_timer_timeout():
-	if pode_atirar and alvo != null:
-		ataque_comp.Ataque(global_position.x, global_position.y)
+	if $AtaqueComp.projetil and pode_atirar and Alvo != null:
+		$AtaqueComp.Ataque(global_position.x, global_position.y)
 		pode_atirar = false
 		fire_timer.start()
+	pass
+
 
 func morrer():
-	emit_signal("enemy_destroyed", self)
+	if animacoes:
+		animacoes.play("morte")
+		$Animações.connect("animation_finished", self._on_morte_animacao_finalizada)
+	else:
+		queue_free()
+
+func _on_morte_animacao_finalizada():
+	emit_signal("inimigo_morto", self)
 	queue_free()
 
 func _on_area_entered(area):
 	if area.has_method("receber_dano"):
 		area.receber_dano(dano_colisao)
 	vida_comp.Dano(vida_comp.VidaAtual)
+
+func _on_fire_timeout():
+	pode_atirar = true
