6c155e76a48b28018146d8c6c0eba3baa90053ce
Vicente <vincentius.ptj@gmail.com>
2024-07-05 13:19:39 -0300

    Melhorias no funcionamento

--- Projeteis/Jogador/AtaqueBase.gd
+++ Projeteis/Jogador/AtaqueBase.gd
@@ -1,8 +1,12 @@
 extends ProjetilComp
 
+@onready var audio_stream_player = $AudioStreamPlayer
+@export var volume: float = -4.0
 @export_range(-180,180) var rotacao: float
 
 func _ready():
+	audio_stream_player.volume_db = volume
+	audio_stream_player.play()
 	rotacao = deg_to_rad(rotacao)
 	pass
 
