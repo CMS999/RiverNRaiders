d502f95726541d82e8b5004f0ef38da71b115379
CMS999 <cauams113@gmail.com>
2024-07-18 17:07:09 -0300

    Warning fix 1 (apresentação 1)

--- Projeteis/Jogador/AnimacaoMissel.gd
+++ Projeteis/Jogador/AnimacaoMissel.gd
@@ -12,5 +12,5 @@ func playExplosao():
 	play("Explosao")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _process(_delta):
 	pass
--- UI/UIJogador.gd
+++ UI/UIJogador.gd
@@ -19,7 +19,7 @@ func _ready():
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _process(_delta):
 	pontos.text = "Score: " +  str(GlobalReference.pontuacao)
 	vida.text = "Vidas: " +    str(GlobalReference.vidas)
 	pass
